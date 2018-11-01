3774c94755510a368d77ab987325f8c98da1101c
NathanielYoung <young.nathaniel95@gmail.com>
2018-11-01 11:27:07 -0600

    Tech costs add part 5

--- src/components/technologyMenu.js
+++ src/components/technologyMenu.js
@@ -48,14 +48,20 @@ class TechnologyMenu extends Component {
                         <div className="techType">
                             Home Tech
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('home', 't1', 1, 'Tents')}>
+                        <div className="technologys" onClick={() => this.Unlock('home', 't1', 0, 'Tents')}>
                             {this.props.unlocklocks[unl + 'home' + 't1']} Tents
+                            <img src="./assets/res.svg"></img>
+                            0
                         </div>
                         <div className="technologys" onClick={() => this.Unlock('home', 't2', 6, 'Cabins')}>
                             {this.props.unlocklocks[unl + 'home' + 't2']} Cabins
+                            <img src="./assets/res.svg"></img>
+                            6
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('home', 't3', 3, 'Brick Home')}>
+                        <div className="technologys" onClick={() => this.Unlock('home', 't3', 15, 'Brick Home')}>
                             {this.props.unlocklocks[unl + 'home' + 't3']} Brick Homes
+                            <img src="./assets/res.svg"></img>
+                            15
                         </div>
                     </div>
 
@@ -63,14 +69,20 @@ class TechnologyMenu extends Component {
                         <div className="techType">
                             Store Tech
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('store', 't1', 1, 'Market')}>
+                        <div className="technologys" onClick={() => this.Unlock('store', 't1', 0, 'Market')}>
                             {this.props.unlocklocks[unl + 'store' + 't1']} Market
+                            <img src="./assets/res.svg"></img>
+                            0
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('store', 't2', 2, 'Dinner')}>
+                        <div className="technologys" onClick={() => this.Unlock('store', 't2', 5, 'Dinner')}>
                             {this.props.unlocklocks[unl + 'store' + 't2']} Dinner
+                            <img src="./assets/res.svg"></img>
+                            5
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('store', 't3', 3, 'Super Market')}>
+                        <div className="technologys" onClick={() => this.Unlock('store', 't3', 17, 'Super Market')}>
                             {this.props.unlocklocks[unl + 'store' + 't3']} Super Market
+                            <img src="./assets/res.svg"></img>
+                            17
                         </div>
                     </div>
 
@@ -78,14 +90,20 @@ class TechnologyMenu extends Component {
                         <div className="techType">
                             Camp Tech
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('camp', 't1', 1, 'BootCamp')}>
+                        <div className="technologys" onClick={() => this.Unlock('camp', 't1', 0, 'BootCamp')}>
                             {this.props.unlocklocks[unl + 'camp' + 't1']} Bootcamp
+                            <img src="./assets/res.svg"></img>
+                            0
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('camp', 't2', 2, 'Military Base')}>
+                        <div className="technologys" onClick={() => this.Unlock('camp', 't2', 8, 'Military Base')}>
                             {this.props.unlocklocks[unl + 'camp' + 't2']} Military Base
+                            <img src="./assets/res.svg"></img>
+                            8
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('camp', 't3', 3, 'Air Field')}>
+                        <div className="technologys" onClick={() => this.Unlock('camp', 't3', 20, 'Air Field')}>
                             {this.props.unlocklocks[unl + 'camp' + 't3']} Air Field
+                            <img src="./assets/res.svg"></img>
+                            20
                         </div>
                     </div>
 
@@ -93,14 +111,20 @@ class TechnologyMenu extends Component {
                         <div className="techType">
                             School Tech
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('school', 't1', 1, 'Class Room')}>
+                        <div className="technologys" onClick={() => this.Unlock('school', 't1', 5, 'Class Room')}>
                             {this.props.unlocklocks[unl + 'school' + 't1']} Class Room
+                            <img src="./assets/res.svg"></img>
+                            5
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('school', 't2', 2, 'Public School')}>
+                        <div className="technologys" onClick={() => this.Unlock('school', 't2', 13, 'Public School')}>
                             {this.props.unlocklocks[unl + 'school' + 't2']} Public School
+                            <img src="./assets/res.svg"></img>
+                            13
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('school', 't3', 3, 'University')}>
+                        <div className="technologys" onClick={() => this.Unlock('school', 't3', 22, 'University')}>
                             {this.props.unlocklocks[unl + 'school' + 't3']} University
+                            <img src="./assets/res.svg"></img>
+                            22
                         </div>
                     </div>
 
@@ -108,14 +132,20 @@ class TechnologyMenu extends Component {
                         <div className="techType">
                             Factory Tech
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('factory', 't1', 1)}>
+                        <div className="technologys" onClick={() => this.Unlock('factory', 't1', 6)}>
                             {this.props.unlocklocks[unl + 'factory' + 't1']} Basic Weapons
+                            <img src="./assets/res.svg"></img>
+                            6
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('factory', 't2', 2)}>
+                        <div className="technologys" onClick={() => this.Unlock('factory', 't2', 14)}>
                             {this.props.unlocklocks[unl + 'factory' + 't2']} Advanced Weapons
+                            <img src="./assets/res.svg"></img>
+                            14
                         </div>
-                        <div className="technologys" onClick={() => this.Unlock('factory', 't3', 3)}>
+                        <div className="technologys" onClick={() => this.Unlock('factory', 't3', 21)}>
                             {this.props.unlocklocks[unl + 'factory' + 't3']} Vehicle Facotory
+                            <img src="./assets/res.svg"></img>
+                            21
                         </div>
                     </div>
                 </div>
